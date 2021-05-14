b98552f88a62f45b9537e7c8e2f1e27c6b0ba373
kshv41 <47850993+kshv41@users.noreply.github.com>
2021-05-14 11:30:38 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,10 +6,9 @@ public class Main {
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 	    String doAgain = "yes";
-        while(doAgain.equals("yes"))
-	    {
+        while(doAgain.equals("yes")){
             
-            System.out.print("Enter the number:");
+	    System.out.print("Enter the number:");
             byte input = scanner.nextByte();
 	    if (input % 3 == 0 && input % 5 == 0) System.out.println("FizzBuzz");
             else if (input % 3 == 0) System.out.println("Fizz");
